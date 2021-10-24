1d3e1b5f23bcd64238e0d74aca7c57bd4b88a88e
hrmsk66 <hrmsk66@gmail.com>
2021-10-24 16:09:51 +0900

    Add robots.txt handling

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 // This example Compute@Edge app modifies HTML returned from https://example.com at the edge
 use fastly::http::{header, StatusCode};
 use fastly::{Error, Request, Response};
-use lol_html::{rewrite_str, element, text, RewriteStrSettings};
 use lol_html::html_content::ContentType;
+use lol_html::{element, rewrite_str, text, RewriteStrSettings};
 
 // Load files into the constants
 const FONT_LINKS: &str = include_str!("font.html");
@@ -10,6 +10,10 @@ const STYLE: &str = include_str!("style.css");
 
 #[fastly::main]
 fn main(mut req: Request) -> Result<Response, Error> {
+    // Tell robots not to index
+    if req.get_url().path() == "/robots.txt" {
+        return Ok(Response::from_status(StatusCode::OK).with_body("User-agent: *\nDisallow: /"));
+    }
     // Add the host header so that you don't need to specify it in a request when testing locally
     req.set_header(header::HOST, "example.com");
     // Request an uncompressed response
@@ -41,7 +45,8 @@ fn rewrite_html(beresp: Response) -> Response {
         // "<h1>Example Domain</h1>" -> "<h1><span>Example</span><span>Domain</span></h1>"
         text!("h1", |t| {
             if !t.last_in_text_node() {
-                let tagged_t = t.as_str()
+                let tagged_t = t
+                    .as_str()
                     .split(" ")
                     .map(|w| format!("<span>{}</span>", w))
                     .fold(String::new(), |mut acc, cur| {
@@ -54,10 +59,14 @@ fn rewrite_html(beresp: Response) -> Response {
         }),
     ];
 
-    let html = rewrite_str(&beresp.into_body_str(), RewriteStrSettings {
-        element_content_handlers,
-        ..RewriteStrSettings::default()
-    }).unwrap();
+    let html = rewrite_str(
+        &beresp.into_body_str(),
+        RewriteStrSettings {
+            element_content_handlers,
+            ..RewriteStrSettings::default()
+        },
+    )
+    .unwrap();
 
     resp.with_body(html)
 }
